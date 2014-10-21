a8e6568cb2155a20d3f228048807bad5e362eea9
ccoenraets <ccoenraets@gmail.com>
2014-10-21 10:39:17 -0400

    First Commit

---
+++ bin/force-server.js
@@ -0,0 +1,43 @@
+#!/usr/bin/env node
+
+var express = require('express'),
+    path = require('path'),
+    request = require('request'),
+    bodyParser = require('body-parser'),
+    app = express();
+
+app.use(bodyParser.json());
+
+app.use(express.static(path.join(__dirname, '.')));
+
+app.all('*', function (req, res, next) {
+
+    // Set CORS headers: allow all origins, methods, and headers: you may want to lock this down in a production environment
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Methods", "GET, PUT, PATCH, POST, DELETE");
+    res.header("Access-Control-Allow-Headers", req.header('access-control-request-headers'));
+
+    if (req.method === 'OPTIONS') {
+        // CORS Preflight
+        res.send();
+    } else {
+        var targetURL = req.header('Target-URL');
+        if (!targetURL) {
+            res.send(500, { error: 'There is no Target-Endpoint header in the request' });
+            return;
+        }
+        request({ url: targetURL + req.url, method: req.method, json: req.body, headers: {'Authorization': req.header('Authorization')} },
+            function (error, response, body) {
+                if (error) {
+                    console.error('error: ' + response.statusCode)
+                }
+//                console.log(body);
+            }).pipe(res);
+    }
+});
+
+app.set('port', process.env.PORT || 3000);
+
+app.listen(app.get('port'), function () {
+    console.log('Proxy server listening on port ' + app.get('port'));
+});
